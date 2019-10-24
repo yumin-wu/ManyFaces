5b1727db5ff9c6e91cad309ff2a2be51ce8d0eb6
yumin-wu <yuminwu1997@gmail.com>
2019-10-23 21:59:56 -0400

    add Logo component with react-tilt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* {<Logo />
-      <ImageLinkForm />
-      <FaceRecognition />} */}
+      <Logo />
+      {/* <ImageLinkForm />
+      <FaceRecognition /> */}
     </div>
   );
 }
