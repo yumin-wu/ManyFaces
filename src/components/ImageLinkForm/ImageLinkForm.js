83408bd52833f40151866810c636601e9b1708a3
yumin-wu <yuminwu1997@gmail.com>
2019-10-24 23:46:50 -0400

    make ImagineLinkForm prettier

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ImageLinkForm.css';
 
 const ImageLinkForm = () => {
     return (
@@ -6,9 +7,11 @@ const ImageLinkForm = () => {
             <p className='f3'>
                 {'This Magic Machine will detect faces in your picture. Give it a try.'}
             </p>
-            <div>
-                <input className='f4 pa2 w-70 center' type='tex' />
-                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+            <div className="center">
+                <div className="form center pa4 br3 shadow-5">
+                    <input className='f4 pa2 w-70 center' type='tex' />
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+                </div>
             </div>
         </div>
     );
