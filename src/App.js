8a50a0d2a142ac9f7c431dd01cee4b3870d5685e
yumin-wu <yuminwu1997@gmail.com>
2019-10-25 23:47:14 -0400

    add rank element and particle effect in background

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from "react";
+import Particles from 'react-particles-js';
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
 import "./App.css";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles"
+        params={particlesOptions}
+      />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
