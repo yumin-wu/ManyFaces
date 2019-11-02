6334c5c5c997041155bbb83f2addd3c5a44d9da4
yumin-wu <yuminwu1997@gmail.com>
2019-11-02 16:19:19 -0400

    add clarifai starter code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
-import Navigation from "./components/Navigation/Navigation";
-import Logo from "./components/Logo/Logo";
-import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
-import Rank from "./components/Rank/Rank";
-import "./App.css";
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: '15c232ac55904a45a9924213e3306665'
+});
 
 const particlesOptions = {
   particles: {
@@ -40,6 +45,15 @@ class App extends Component {
 
   onButtonSubmit = (event) => {
     console.log(event.target.value);
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+      function (response) {
+        // do something with response
+        console.log(response);
+      },
+      function (err) {
+        // there was an error
+      }
+    );
   }
 
   render() {
